3ac440e9c6b34d8952591189d8397f368a115e0d
Oraelys <alanamos0001@gmail.com>
2025-03-13 07:42:49 +0100

    13-03-2025
    Worked on the testimonials section both left and right side

--- restuarant-app/src/App.jsx
+++ restuarant-app/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import NavBar from './components/NavBar'
+import Banner from './components/Banner'
+import Wws from './components/Wws'
+import Om from './components/Om'
+import Wts from './components/Wts'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function app() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='min-h-screen flex flex-col overflow-x-hidden bg-gradient-to-r from-red-50 via-orange-50 to-yellow-50'>
+      <NavBar />
+      <br />
+
+      <main className='flex-1 w-full'>
+        <div className="w-full">
+          <Banner />
+        </div>
+        <div className="w-full">
+          <Wws />
+          <Om />
+          <Wts />
+        </div>
+      </main>
+    </div>
   )
 }
 
-export default App
+export default app
